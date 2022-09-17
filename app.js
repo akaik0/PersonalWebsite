f6a776f9b181e5ea8d8cb58cba365428d17f2e42
Arina Kosiakov <akaiko.social@gmail.com>
2022-09-17 19:12:03 +0200

    adjustment homepage

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
-const header = document.getElementById('fade')
-const arrow = document.getElementById('scroll')
+const wallpaper = document.querySelector('.wallpaper')
+const nav = document.querySelector('nav')
+
+wallpaper.style.height = window.screen.availHeight;
+
 window.addEventListener('scroll', function () {
-    header.style.opacity = 1 - +this.window.pageYOffset / 1000 + ''
-    arrow.style.opacity = 1 - +this.window.pageYOffset / 1000 + ''
+    wallpaper.style.opacity = 1 - +this.window.pageYOffset / 1000 + ''
 })
 
-const faders = document.querySelectorAll('.fade-in');
-const appearOptions = {
-    threshhold: 1,
-    rootMargin: '0px 0px -400px 0px'
-}
-const appearOnSCroll = new IntersectionObserver
-    (function (entries, appearOnSCroll) {
-        entries.forEach(entry => {
-            if (!entry.isIntersecting) {
-                return;
-            }
-            else {
-                entry.target.classList.add('appear');
-                appearOnSCroll.unobserve(entry.target);
-            }
-        })
-    },
-        appearOptions);
-
-faders.forEach(fader => {
-    appearOnSCroll.observe(fader);
+window.addEventListener('scroll', function () {
+    nav.style.opacity = 0 + +this.window.pageYOffset / 750 + ''
 })
